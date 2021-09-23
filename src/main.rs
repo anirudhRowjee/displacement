bdeb465d73c6f68ae2769929aff2ed59ff557d06
Anirudh Rowjee <ani.rowjee@gmail.com>
2021-09-23 19:15:10 +0530

    added simple hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
+/*
+ * Displacement: A simple implementation of the FTP protocol
+ */
+
+use std::io;
+use termion::raw::IntoRawMode;
+
+use tui::Terminal;
+use tui::backend::TermionBackend;
+// use tui::widgets::{Widget, Block, Borders};
+use tui::widgets::{Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
+
+
+fn main() -> Result<(), io::Error> {
+
     println!("Hello, world!");
+
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+
+        // split the layout into three vertical chunks according
+        // to the constraints provided
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10)
+                ].as_ref()
+            )
+            .split(f.size());
+
+        // create a basic block
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+
+        // render
+        f.render_widget(block, chunks[0]);
+
+        let block = Block::default()
+            .title("Block 2")
+            .borders(Borders::ALL);
+
+        // render
+        f.render_widget(block, chunks[1]);
+
+        let block = Block::default()
+            .title("Block 3")
+            .borders(Borders::ALL);
+
+        // render
+        f.render_widget(block, chunks[2]);
+
+    })?;
+    Ok(())
 }
