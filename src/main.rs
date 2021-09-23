c8c10740739d735119dfda018075a84c4a21c921
NavinShrinivas <karupal2002@gmail.com>
2021-09-23 20:06:53 +0530

    Cleaner stuff for review-1

--- src/main.rs
+++ src/main.rs
@@ -5,59 +5,64 @@
 use std::io;
 use termion::raw::IntoRawMode;
 
-use tui::Terminal;
-use tui::backend::TermionBackend;
-// use tui::widgets::{Widget, Block, Borders};
-use tui::widgets::{Block, Borders};
-use tui::layout::{Layout, Constraint, Direction};
-
+use tui::{
+    Terminal,
+    layout::{Constraint,Direction,Layout,Alignment},
+    backend::TermionBackend,
+    widgets::{Block,Borders,Paragraph},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans}
+};
 
 fn main() -> Result<(), io::Error> {
-
     println!("Hello, world!");
 
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-
+    let text = vec![
+        Spans::from(""),
+        Spans::from(""), //simply to push content into bloc, very bad practice.
+        Spans::from(Span::styled("FTP using Rust" , Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)))
+    ];
     terminal.draw(|f| {
-
         // split the layout into three vertical chunks according
         // to the constraints provided
+        let para = Paragraph::new(text.clone())
+        .style(Style::default().bg(Color::White))
+        .alignment(Alignment::Center);
         let chunks = Layout::default()
             .direction(Direction::Vertical)
             .margin(1)
             .constraints(
                 [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10)
-                ].as_ref()
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(60),
+                    Constraint::Percentage(20),
+                ]
+                .as_ref(),
             )
             .split(f.size());
 
         // create a basic block
-        let block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
+        let block = Block::default().title("Block").borders(Borders::ALL)
+            .style(Style::default().bg(Color::White));
 
         // render
         f.render_widget(block, chunks[0]);
 
-        let block = Block::default()
-            .title("Block 2")
-            .borders(Borders::ALL);
+        let block = Block::default().title("Block 2").borders(Borders::ALL)
+            .style(Style::default().bg(Color::White));
 
         // render
         f.render_widget(block, chunks[1]);
 
-        let block = Block::default()
-            .title("Block 3")
-            .borders(Borders::ALL);
+        let block = Block::default().title("Block 3").borders(Borders::ALL)
+            .style(Style::default().bg(Color::White));
 
         // render
         f.render_widget(block, chunks[2]);
-
+        f.render_widget(para, chunks[0]);
     })?;
     Ok(())
 }
